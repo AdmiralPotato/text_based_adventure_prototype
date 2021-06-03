95ad386b0883a626ba7298ef8b05fea9b2caf493
Admiral Potato <admiral@nuclearpixel.com>
2021-06-02 20:23:38 -0600

    Implement verb: go

--- index.js
+++ index.js
@@ -29,30 +29,70 @@ var help = function () {
 	copyToBuffer(output);
 };
 
-var currentRoom = {
+var duzzy = {
 	description: [
-		'You have entered the Dungeon.',
-		'',
-		'It looks like there was some sophistocated',
-		'security system that should have kept out',
-		'any intruders, but there is a power switch',
-		'next to it that has clearly been left off.',
+		'He looks menacing.',
 	].join('\n'),
-	objects: {
-		flask: {
-			description: [
-				'The flask looks like it can hold 1.2 litres',
-				'worth of fluids. Someone has laser-etched',
-				'a PipsCat on to it.',
-			].join('\n'),
+};
+
+var rooms = {
+	start: {
+		description: [
+			'You have entered the Dungeon.',
+			'',
+			'It looks like there was some sophistocated',
+			'security system that should have kept out',
+			'any intruders, but there is a power switch',
+			'next to it that has clearly been left off.',
+		].join('\n'),
+		objects: {
+			flask: {
+				description: [
+					'The flask looks like it can hold 1.2 litres',
+					'worth of fluids. Someone has laser-etched',
+					'a PipsCat on to it.',
+				].join('\n'),
+			},
+			duzzy
 		},
-		duzzy: {
-			description: [
-				'He looks menacing.',
-			].join('\n'),
-		}
+		directions: {
+			north: 'main_hall_north',
+			south: 'goat_room',
+		},
+	},
+	main_hall_north: {
+		description: [
+			'You are in Main Hall North.',
+			'There is nothing but filth here.',
+			'Broken devices, empty Cactus Cooler cans, ',
+			'and smelly Smash Bros. players.',
+		].join('\n'),
+		objects: {
+			duzzy
+		},
+		directions: {
+			south: 'start',
+		},
+	},
+	goat_room: {
+		description: [
+			'You are in the Goat Room.',
+			'There is a wise old Goat here.',
+		].join('\n'),
+		objects: {
+			goat: {
+				description: [
+					'The Goat looks very wise.',
+				].join('\n'),
+			}
+		},
+		directions: {
+			north: 'start',
+		},
 	},
 };
+
+var currentRoom = rooms.start;
 
 var look = function (targetString) {
 	var target = currentRoom;
@@ -68,15 +108,39 @@ var look = function (targetString) {
 				Object.keys(target.objects)
 			);
 		}
+		if (target.directions) {
+			output += '\n\nExits are:\n';
+			output += listOptions(
+				Object.keys(target.directions)
+			);
+		}
 	} else {
 		output += `You try to look at ${targetString},\nbut only the void stares back.`
 	}
 	copyToBuffer(output);
 };
 
+var go = function (targetString) {
+	var output = '';
+	var newRoom;
+	if (targetString) {
+		var destinationRoomName = currentRoom.directions[targetString];
+		var newRoom = rooms[destinationRoomName];
+		if (newRoom) {
+			currentRoom = newRoom;
+			look();
+		} else {
+			output += `You cannot go "${targetString}"`;
+		}
+	} else {
+		output += 'You cannot go nowhere';
+	}
+	copyToBuffer(output);
+};
+
 var verbs = {
 	look,
-	go: null,
+	go,
 	get: null,
 	bag: null,
 	inventory: null,
@@ -93,7 +157,7 @@ verbs.look();
 var processCommand = function (commandString) {
 	var segments = commandString.split(' ');
 	var verbString = segments[0].toLocaleLowerCase();
-	var targetString = segments[1];
+	var targetString = (segments[1] || '').toLocaleLowerCase();
 	var verb = verbs[verbString];
 	if (verb) {
 		copyToBuffer('> ' + commandString);
